ed94aba2141112413a897522901d47c56566ee8a
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-09-15 17:24:14 -0500

    Add HTML skeleton, header, add book button

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${
+      read ? 'already read' : 'not read yet'
+    }`;
+  };
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+
+console.log(theHobbit.info());
