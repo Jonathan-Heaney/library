e135c9ece955aeb5cec5a418010763ae7e1f934b
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-09-16 11:58:48 -0500

    Add form modal with overlay

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 'use strict';
 
+const openAddBook = document.getElementById('add-btn');
+const overlay = document.getElementById('overlay');
+const modal = document.getElementById('modal');
+
+openAddBook.addEventListener('click', () => {
+  openModal(modal);
+});
+
+overlay.addEventListener('click', () => {
+  closeModal(modal);
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
